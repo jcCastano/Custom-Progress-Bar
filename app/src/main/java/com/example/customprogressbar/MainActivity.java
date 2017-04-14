9556ddd82ccc43c07b25e3d8893b3c4f2f459f0e
Jc Castano <jc.castano@goemerchant.com>
2017-04-14 13:56:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/customprogressbar/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.customprogressbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    EditText progressNum;
+    Button updateBtn;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressNum = (EditText) findViewById(R.id.progressNum);
+        updateBtn = (Button) findViewById(R.id.updateBtn);
+        updateBtn.setOnClickListener(updateClickedListener);
+    }
+
+    View.OnClickListener updateClickedListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String num = progressNum.getText().toString();
+            try {
+                int progress = Integer.parseInt(num);
+                if (progress > 0 && progress <= 100)
+                    progressBar.setProgress(progress);
+                else
+                    Toast.makeText(MainActivity.this, "Please enter a number between 0 and 100", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Please enter a number between 0 and 100", Toast.LENGTH_LONG).show();
+                Log.e(TAG, "onClick: ", e);
+            }
+
+        }
+    };
+
+}
